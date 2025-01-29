5861bd43aaaa8822d5cbc5c9f769131f8e55aee6
Utkarsh Garg <utkarshgarg143@gmail.com>
2025-01-29 18:46:39 +0530

    updated

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema(
 	{
+		title: { type: String, required: true },
+		cover_image: { type: String, required: true },
+		category: { type: String, required: true },
 		content: { type: String, required: true },
-		isPublished: { type: Boolean, default: true },
+		isPublished: { type: Boolean, default: false },
 	},
 	{ timestamps: true }
 );
